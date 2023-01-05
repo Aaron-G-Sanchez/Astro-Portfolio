b1af950ceb590d778f4010095b26c0e2e9526e94
Aaron Sanchez <saarong12@gmail.com>
2023-01-05 14:22:24 -0600

    Converted about section into JSX component

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section class="about">
+      <div class="about-wrapper">
+        <p>
+          As a software engineer with a passion for audio, I bring a unique
+          blend of technical expertise and creative problem-solving to every
+          project. My background in the audio industry has given me a deep
+          understanding of the importance of sound quality and the nuances of
+          audio production. Whether I am working on building new software tools
+          or optimizing existing systems, I am dedicated to delivering
+          innovative solutions that enhance the user experience and exceed
+          expectations.
+        </p>
+      </div>
+      <div class="skills">
+        <h2>Skills</h2>
+      </div>
+    </section>
+  )
+}
+
+export default About
