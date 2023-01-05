d0f0f6b23083e5dc4a60f179a0131ed29ad6f016
Aaron Sanchez <saarong12@gmail.com>
2023-01-05 15:23:10 -0600

    Added social links

---
+++ src/components/Links.jsx
@@ -0,0 +1,29 @@
+const Links = () => {
+  return (
+    <>
+      <nav className="links-bar">
+        <div className="button-container">
+          <a href="https://www.linkedin.com/in/aaron-g-sanchez/">
+            <img
+              className="social-icon"
+              src="/assets/images/linkedin.png"
+              alt="linkedin-logo"
+            />
+          </a>
+        </div>
+
+        <div className="button-container">
+          <a href="https://github.com/Aaron-G-Sanchez">
+            <img
+              className="social-icon"
+              src="/assets/images/github.png"
+              alt="github-logo"
+            />
+          </a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Links
