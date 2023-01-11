a0bfdaa3a637ea3474446f84aa566fdbc8d138ca
Aaron Sanchez <saarong12@gmail.com>
2023-01-11 14:53:14 -0600

    Project section started

---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <section className="projects">
+      <div>
+        <h1>Project Section</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
