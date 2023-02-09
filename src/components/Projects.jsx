6adb3077d1875cc5e97845faec79ca022e26bb94
Aaron Sanchez <saarong12@gmail.com>
2023-02-08 18:31:41 -0600

    Added projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,22 @@
 const Projects = () => {
-  return <section className="projects"></section>
+  return (
+    <>
+      <section className="projects">
+        <p className="projects-title">My Projects</p>
+        <section className="projects-wrapper">
+          <div>
+            <p>Sines Ear Trainer</p>
+          </div>
+          <div>
+            <p>Hogwarts Administration System</p>
+          </div>
+          <div>
+            <p>BeerBook</p>
+          </div>
+        </section>
+      </section>
+    </>
+  )
 }
 
 export default Projects
